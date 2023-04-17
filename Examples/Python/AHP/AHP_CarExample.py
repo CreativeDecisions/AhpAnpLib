d38673550ec1cf71410e5a287dee2ec482e4bf93
Elena <erokou@gmail.com>
2023-04-17 13:22:42 -0400

    Update AHP_CarExample.py

--- Examples/Python/AHP/AHP_CarExample.py
+++ Examples/Python/AHP/AHP_CarExample.py
@@ -3,10 +3,11 @@ from AhpAnpLib import inputs_AHPLib as input
 from AhpAnpLib import structs_AHPLib as str
 from AhpAnpLib import calcs_AHPLib as calc
 from AhpAnpLib import ratings_AHPLib as rate
-import numpy as np
+from AhpAnpLib import functions_AHPLib as reqLib
 
-#set here the number of decimals that you want to be displayed
-np.set_printoptions(formatter={'float': lambda x: "{0:0.3f}".format(x)})
+#You don't need this
+# #set here the number of decimals that you want to be displayed
+# reqLib.np.set_printoptions(formatter={'float': lambda x: "{0:0.3f}".format(x)})
 
 #create model
 carModel=str.Model()
