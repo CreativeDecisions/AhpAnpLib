30111229086d0a59eacdb44e98b3f811dfba260a
Lirong <34753141+weileerong@users.noreply.github.com>
2023-06-12 10:45:20 +0800

    upload dict Exp22-26

---
+++ Examples/Dictionary/dictExp22_removeConnectionsTo.py
@@ -0,0 +1,34 @@
+
+from AhpAnpLib import structs_AHPLib as str
+
+myModel=str.Model("my model")
+# create nodes
+ANode=str.Node("A",0)
+BNode=str.Node("B",1)
+CNode=str.Node("C",2)
+DNode=str.Node("D",3)
+XNode=str.Node("X",4)
+YNode=str.Node("Y",5)
+ZNode=str.Node("Z",6)
+# create clusters
+Cluster1=str.Cluster("One",0)
+Cluster2=str.Cluster("Two",1)
+Cluster3=str.Cluster("Three",1)
+#add nodes to clusters
+Cluster1.addNode2Cluster(ANode) 
+Cluster1.addNode2Cluster(BNode)
+Cluster2.addNode2Cluster(CNode) 
+Cluster2.addNode2Cluster(DNode) 
+Cluster3.addNode2Cluster(XNode)
+Cluster3.addNode2Cluster(YNode)
+Cluster3.addNode2Cluster(ZNode)
+#add clusters to the model 
+myModel.addCluster2Model(Cluster1)
+myModel.addCluster2Model(Cluster2)
+myModel.addCluster2Model(Cluster3)
+#add connections
+myModel.addNodeConnectionFromAllNodesToAllNodesOfCluster("Two","One")
+myModel.addNodeConnectionFromAllNodesToAllNodesOfCluster("Three","One")
+
+myModel.remAllNodeConnectionsTo("B")
+myModel.showAllNodeConnections()
