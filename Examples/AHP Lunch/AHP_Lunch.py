cab5909f415a2611d82c6e2267b7b9d6aa5abbf5
Lirong <34753141+weileerong@users.noreply.github.com>
2023-09-04 11:31:18 +0800

    update models and add jupyter examples

--- Examples/AHP Lunch/AHP_Lunch.py
+++ Examples/AHP Lunch/AHP_Lunch.py
@@ -1,27 +1,27 @@
-from AhpAnpLib import inputs_AHPLib as cdf_inp
-from AhpAnpLib import structs_AHPLib as cdf_str
-from AhpAnpLib import calcs_AHPLib as cdf_calc
+from AhpAnpLib import inputs_AHPLib as input
+from AhpAnpLib import structs_AHPLib as str
+from AhpAnpLib import calcs_AHPLib as calc
 
 #create model
-lunchModel=cdf_str.Model("Lunch")
+lunchModel=str.Model("Lunch")
 print(lunchModel)
 
 #create nodes
-goal_node=cdf_str.Node("GoalNode",0)
+goal_node=str.Node("GoalNode",0)
 
-quality=cdf_str.Node("1Quality",1)
-price=cdf_str.Node("2Price",2)
-menu=cdf_str.Node("3Menu",3)
-speed=cdf_str.Node("4Speed",4)
+quality=str.Node("1Quality",1)
+price=str.Node("2Price",2)
+menu=str.Node("3Menu",3)
+speed=str.Node("4Speed",4)
 
-alt1=cdf_str.Node("1Primanti",5)
-alt2=cdf_str.Node("2Panera",6)
-alt3=cdf_str.Node("3Piada",7)
+alt1=str.Node("1Primanti",5)
+alt2=str.Node("2Panera",6)
+alt3=str.Node("3Piada",7)
 
 #create clusters
-cluster0=cdf_str.Cluster("1Goal",0)
-cluster1=cdf_str.Cluster("2Criteria",1)
-cluster2=cdf_str.Cluster("3Alternatives",2)
+cluster0=str.Cluster("1Goal",0)
+cluster1=str.Cluster("2Criteria",1)
+cluster2=str.Cluster("3Alternatives",2)
 
 #add nodes to clusters
 cluster0.addNode2Cluster(goal_node) 
@@ -44,11 +44,11 @@ lunchModel.addNodeConnectionFromAllNodesToAllNodesOfCluster("2Criteria","3Altern
 # Print out model
 lunchModel.printStruct()
 
-cdf_inp.export4ExcelQuestFull(lunchModel,"lunchModel_Excel_empty.xlsx",True)
+input.export4ExcelQuestFull(lunchModel,"lunchModel_Excel_empty.xlsx",True)
 
 inputFilePath="lunchModel_Excel_filledIn.xlsx"
 outputFilePath="lunchModel_Results.xlsx"
 
-#cdf_inp.importFromExcel(lunchModel,inputFilePath,0)
-cdf_calc.calcAHPMatricesSave2File(lunchModel,inputFilePath,outputFilePath,True,True,True,False)
-cdf_calc.sensitivityCellSupermatrixPlot(lunchModel,"3Alternatives",outputFilePath,False,"1Quality","2Price","3Menu","4Speed")
+#input.importFromExcel(lunchModel,inputFilePath,0)
+calc.calcAHPMatricesSave2File(lunchModel,inputFilePath,outputFilePath,True,True,True,False)
+calc.sensitivityCellSupermatrixPlot(lunchModel,"3Alternatives",outputFilePath,False,"1Quality","2Price","3Menu","4Speed")
