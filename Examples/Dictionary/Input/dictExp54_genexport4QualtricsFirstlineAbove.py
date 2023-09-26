65a1a3764b3bcf373d6aa6f7f07862802a9489aa
Lirong <34753141+weileerong@users.noreply.github.com>
2023-09-26 14:24:15 +0800

    add dict exp Qualtrics quest

---
+++ Examples/Dictionary/Input/dictExp54_genexport4QualtricsFirstlineAbove.py
@@ -0,0 +1,35 @@
+from AhpAnpLib import structs_AHPLib as str
+from AhpAnpLib import inputs_AHPLib as input
+
+myModel=str.Model("my model")
+# create nodes
+GNode=str.Node("G",0)
+ANode=str.Node("A",0)
+BNode=str.Node("B",1)
+CNode=str.Node("C",2)
+DNode=str.Node("D",3)
+XNode=str.Node("X",4)
+YNode=str.Node("Y",5)
+ZNode=str.Node("Z",6)
+# create clusters
+Cluster0=str.Cluster("Goal",0)
+Cluster1=str.Cluster("One",0)
+Cluster2=str.Cluster("Two",1)
+#add nodes to clusters
+Cluster0.addNode2Cluster(GNode)
+Cluster1.addNode2Cluster(ANode) 
+Cluster1.addNode2Cluster(BNode)
+Cluster1.addNode2Cluster(CNode) 
+Cluster1.addNode2Cluster(DNode) 
+Cluster2.addNode2Cluster(XNode)
+Cluster2.addNode2Cluster(YNode)
+Cluster2.addNode2Cluster(ZNode)
+#add clusters to the model 
+myModel.addCluster2Model(Cluster0)
+myModel.addCluster2Model(Cluster1)
+myModel.addCluster2Model(Cluster2)
+# add connections
+myModel.addNodeConnectionFromNodeToAllNodesOfCluster("G","One")
+myModel.addNodeConnectionFromAllNodesToAllNodesOfCluster("One","Two")
+#generate a full questionnaire of the model to .txt file that can be imported in Qualtrics survey
+Questionnaire= input.genexport4QualtricsFirstLineAboveDiagQuest("Exp54_QualtricsFistlineandAbove.txt",myModel,"important",True)
