047da4247e3424274dbad54ec0400a086eea193c
Lirong <34753141+weileerong@users.noreply.github.com>
2023-10-04 11:34:23 +0800

    add excel ratings scale py

---
+++ Examples/Dictionary/Input/dictExp60_exportExcelRatingScale.py
@@ -0,0 +1,57 @@
+from AhpAnpLib import structs_AHPLib as str
+from AhpAnpLib import inputs_AHPLib as input
+from AhpAnpLib import ratings_AHPLib as rate
+from AhpAnpLib import calcs_AHPLib as calc
+
+myModel=str.Model("my model")
+# create nodes
+GNode=str.Node("G",0)
+ANode=str.Node("A",0)
+BNode=str.Node("B",1)
+CNode=str.Node("C",2)
+DNode=str.Node("D",3)
+XNode=str.Node("X",4)
+YNode=str.Node("Y",5)
+ZNode=str.Node("Z",6)
+# create clusters
+Cluster0=str.Cluster("Goal",0)
+Cluster1=str.Cluster("One",0)
+Cluster2=str.Cluster("Two",1)
+#add nodes to clusters
+#add nodes to clusters
+Cluster0.addNode2Cluster(GNode)
+Cluster1.addNode2Cluster(ANode) 
+Cluster1.addNode2Cluster(BNode)
+Cluster1.addNode2Cluster(CNode) 
+Cluster1.addNode2Cluster(DNode) 
+Cluster2.addNode2Cluster(XNode)
+Cluster2.addNode2Cluster(YNode)
+Cluster2.addNode2Cluster(ZNode)
+#add clusters to the model 
+myModel.addCluster2Model(Cluster0)
+myModel.addCluster2Model(Cluster1)
+myModel.addCluster2Model(Cluster2)
+# add connections
+myModel.addNodeConnectionFromNodeToAllNodesOfCluster("G","One")
+myModel.addNodeConnectionFromAllNodesToAllNodesOfCluster("One","Two")
+#generate a full questionnaire of the model and export the pairwise comparison matrix to an Excel file
+#we can set to show the estimated priorities in the excel using the third parameter show_estimates as True
+#we can set the verb as True in the fourth parameter to see the process print out in the screen while export excel
+input.export4ExcelQuestFull(myModel,"Example60_Excel_FullQuestionnaire_empty.xlsx",True,True)
+calc.calcAHPMatricesSave2File(myModel,"Example60_Excel_FullQuestionnaire_filledin.xlsx","Example60_Excel_CriteriaPriorities.xlsx",True,False,True)
+
+myModel.setModelTypeRatings()
+
+#add alternatives for ratings
+myModel.rateModel.addAlternativesByName("I","II","III")
+#add criteria for ratings
+myModel.rateModel.addCriteriaByVar(YNode,ZNode)
+#create scale
+modelScale=rate.RatScale("ModelScale")
+modelScale.defineScaleByValue(None,False,"Good","Fair","Poor")
+#add scale to the model
+myModel.rateModel.addScaleByVar(modelScale)
+#assign scales to each criterion
+myModel.rateModel.assignScale2CriterionByName("Y","ModelScale")
+myModel.rateModel.assignScale2CriterionByName("Z","ModelScale")
+input.export4ExcelRatingScales(myModel,"Exp60_ModelScale_empty.xlsx",False,True,False)
