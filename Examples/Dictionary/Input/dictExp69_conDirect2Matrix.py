f456c0c1d7e377254da9883fb00ac5b3e20a8f28
Lirong <34753141+weileerong@users.noreply.github.com>
2023-10-25 21:40:22 +0800

    add dict exp 69-72

---
+++ Examples/Dictionary/Input/dictExp69_conDirect2Matrix.py
@@ -0,0 +1,4 @@
+from AhpAnpLib import inputs_AHPLib as input
+
+directVector = [30,60,80,100]
+input.convDirect2PairwiseMatrix(directVector,True)
---
+++ Examples/Dictionary/Input/dictExp72_readSDMODfile.py
@@ -0,0 +1,3 @@
+from AhpAnpLib import inputs_AHPLib as input
+
+carModel=input.readSDMODfile("Example 72 Car Model","Exp72_CarModel.sdmod",True,False)
