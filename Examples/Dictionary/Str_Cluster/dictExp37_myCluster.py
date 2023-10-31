3e1d51e464f49ab580a6281304a67a974c073caa
Lirong <34753141+weileerong@users.noreply.github.com>
2023-10-31 16:01:30 +0800

    update dict exps

--- Examples/Dictionary/Str_Cluster/dictExp37_myCluster.py
+++ Examples/Dictionary/Str_Cluster/dictExp37_myCluster.py
@@ -4,4 +4,4 @@ myCluster=str.Cluster("my cluster",0)
 Cluster2=str.Cluster("Quality",1) 
 ACluster=str.Cluster("A",3) 
 
-print(myCluster,Cluster2,ACluster)
+print(myCluster,'\n',Cluster2,'\n',ACluster)
--- Examples/Dictionary/Str_Node/dictExp35_parentCluster.py
+++ Examples/Dictionary/Str_Node/dictExp35_parentCluster.py
@@ -6,6 +6,6 @@ ANode=str.Node("Node A",3)
 myCluster=str.Cluster("my cluster",0)
 
 myCluster.addNode2Cluster(Node1)
-
-print(f"Parent cluster of Node1 is: {Node1.myParentCluster()}")
+par1= Node1.myParentCluster()
+print(f"Parent cluster of Node1 is: {par1}")
 print(f"Parent cluster of ANode is: {ANode.myParentCluster()}")
