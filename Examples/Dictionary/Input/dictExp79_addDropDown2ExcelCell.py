b0362a6ef9eb7b3c2bf43a60f95ac4231edd9e8c
Lirong <34753141+weileerong@users.noreply.github.com>
2023-11-01 18:04:44 +0800

    add dict exp files

--- Examples/Dictionary/Input/dictExp79_addDropDown2ExcelCell.py
+++ Examples/Dictionary/Input/dictExp79_addDropDown2ExcelCell.py
@@ -5,5 +5,5 @@ workbook = reqLib.openpyxl.load_workbook("Exp79_Excel_addDropDown2Excel.xlsx")
 workbook.active = workbook.sheetnames.index("Sheet2")
 worksheet = workbook.active
 
-worksheet=input.addDropDown2ExcelCell(worksheet,"B2","B5","Sheet2","B2")
+worksheet=input.addDropDown2ExcelCell(worksheet,"B2","B5","Sheet1","B2")
 workbook.save("Exp79_Excel_addDropDown2Excel.xlsx")
