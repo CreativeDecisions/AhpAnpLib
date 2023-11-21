328e9e31831275543e928de429cec988185deebe
Lirong <34753141+weileerong@users.noreply.github.com>
2023-11-21 09:43:36 +0800

    update dict ex 100

--- Examples/Dictionary/Calc/dictExp100_calcInconsistency.py
+++ Examples/Dictionary/Calc/dictExp100_calcInconsistency.py
@@ -4,5 +4,4 @@ pwMatrix = reqLib.np.array([[1,	0.25,	0.33,	0.50],
 [4,	1,	3.00,	1.50],
 [3,	0.333,	1,	0.33],
 [2,	0.667,	3,	1]])
-inconsistency = calc.calcInconsistency(pwMatrix)
-print(inconsistency)
+inconsistency = calc.calcInconsistency(pwMatrix, True)
