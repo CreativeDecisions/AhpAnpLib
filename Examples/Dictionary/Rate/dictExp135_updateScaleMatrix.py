a2f70252bee41ac69fe4ec5a6713a1321986de6e
Lirong <34753141+weileerong@users.noreply.github.com>
2023-12-20 09:21:33 +0800

    add dictionary examples 134-135 python

---
+++ Examples/Dictionary/Rate/dictExp135_updateScaleMatrix.py
@@ -0,0 +1,50 @@
+from AhpAnpLib import structs_AHPLib as str
+from AhpAnpLib import ratings_AHPLib as rate
+from AhpAnpLib import functions_AHPLib as reqLib
+
+myModel=str.Model("my model")
+# create nodes
+GNode=str.Node("G",0)
+ACri=str.Node("A",0)
+BCri=str.Node("B",1)
+XsubCri=str.Node("X subCri",4)
+YsubCri=str.Node("Y subCri",5)
+ZsubCri=str.Node("Z subCri",6)
+# create clusters
+Cluster0=str.Cluster("Goal",0)
+Cluster1=str.Cluster("One",1)
+Cluster2=str.Cluster("Two",2)
+
+#add nodes to clusters
+Cluster0.addNode2Cluster(GNode)
+Cluster1.addMultipleNodes2Cluster(ACri,BCri)  
+Cluster2.addMultipleNodes2Cluster(XsubCri,YsubCri,ZsubCri)
+#add clusters to the model 
+myModel.addMultipleClusters2Model(Cluster0,Cluster1,Cluster2)
+# add connections
+myModel.addNodeConnectionFromNodeToAllNodesOfCluster("G","One")
+myModel.addNodeConnectionFromNodeToNodeList("A","X subCri","Y subCri")
+myModel.addNodeConnectionFromTo("B","Z subCri")
+
+#set up rating model
+myModel.setModelTypeRatings()
+#add criteria for ratings
+myModel.rateModel.addCriteriaByVar(XsubCri,YsubCri,ZsubCri)
+#add alternatives for ratings
+myModel.rateModel.addAlternativesByName("Alt 1", "Alt 2", "Alt 3", "Alt4")
+
+#define scales
+scale1=rate.RatScale("ExampleScale1")
+scale1.defineScaleByValue(None,False,
+["High", 90],
+["Medium",60], 
+["Low",30]
+)
+
+myModel.rateModel.addScaleByVar(scale1)
+print("scale1 before updating: ",scale1.val_mat)
+newpw = reqLib.np.array([[1,2,4],
+        [0.5,1,2],
+        [0.25,0.5,1]])
+myModel.rateModel.updateScaleMatrix("ExampleScale1", newpw)
+print("scale1 after updating: ",scale1.val_mat)
