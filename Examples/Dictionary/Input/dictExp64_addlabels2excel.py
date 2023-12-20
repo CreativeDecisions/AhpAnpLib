a635714820bf9aca3c815e57df5ebcb0fc6ea0dc
Lirong <34753141+weileerong@users.noreply.github.com>
2023-12-20 20:41:35 +0800

    update addlabels2excel python example

--- Examples/Dictionary/Input/dictExp64_addlabels2excel.py
+++ Examples/Dictionary/Input/dictExp64_addlabels2excel.py
@@ -1,6 +1,6 @@
 from AhpAnpLib import inputs_AHPLib as input
 
-columnlabellist = ["a","b","c","d"]
-rowlabellist = ["x","y","z"]
-input.addLabels2Excel(columnlabellist,"dictExp64_addLabels2excel.xlsx","Sheet1",2,1,0)
-input.addLabels2Excel(rowlabellist,"dictExp64_addLabels2excel.xlsx","Sheet1",1,2,1)
+rowheading = ["a","b","c","d"]
+columnheading = ["x","y","z"]
+input.addLabels2Excel(rowheading,"dictExp64_addLabels2excel.xlsx","Sheet1",2,1,0)
+input.addLabels2Excel(columnheading,"dictExp64_addLabels2excel.xlsx","Sheet1",1,2,1)
