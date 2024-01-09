fd58ffadbca6272479b087a70be1d2a77731d8cd
Lirong <34753141+weileerong@users.noreply.github.com>
2024-01-09 11:16:00 +0800

    rename readRCP file example

--- Examples/Dictionary/Input/dictExp71_readRatScaleRPCfile.py
+++
@@ -1,5 +0,0 @@
-from AhpAnpLib import inputs_AHPLib as input
-from AhpAnpLib import ratings_AHPLib as rate
-from AhpAnpLib import structs_AHPLib as str
-
-scale1=input.readRatScaleRCPfile("Excellent2Poor_Scale","Excellent2Poor.rcp",True)
