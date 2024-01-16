a88ac84fdeb6029ba186845dbbe985961c169216
Lirong <34753141+weileerong@users.noreply.github.com>
2024-01-16 12:28:33 +0800

    test import supermatrix

--- Examples/Dictionary/Input/dictExpN_readSupermatrixFromExcel.py
+++ Examples/Dictionary/Input/dictExpN_readSupermatrixFromExcel.py
@@ -8,4 +8,6 @@ mymodel=str.Model("My Model")
 
 input.readSupermatrixFromExcel(mymodel,'ExpN_readSupermatrix.xlsx',"Sheet1", False)
 
-mymodel.printStruct()
+input.export4ExcelQuestFull(mymodel,"ExpN_readSupermatrix_pw_empty.xlsx")
+
+calc.calcAHPMatricesSave2File(mymodel,"ExpN_readSupermatrix_pw_empty.xlsx","ExpN_readSupermatrix_pw_export.xlsx",False,False,True,True)
