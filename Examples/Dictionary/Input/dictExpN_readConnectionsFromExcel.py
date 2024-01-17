1288bad47683e4733e97a70b5eac0bc69e6a0801
Lirong <34753141+weileerong@users.noreply.github.com>
2024-01-17 12:36:34 +0800

    update dictionary examples

--- Examples/Dictionary/Input/dictExpN_readConnectionsFromExcel.py
+++ Examples/Dictionary/Input/dictExpN_readConnectionsFromExcel.py
@@ -7,6 +7,6 @@ from AhpAnpLib import calcs_AHPLib as calc
 mymodel=str.Model("My Model")
 
 input.readStructFromExcel(mymodel,'ExpN_readconnectionsstruct_Excel.xlsx',"Sheet2", False)
-input.readConnectionsFromExcel(mymodel,'ExpN_readconnectionsstruct_Excel.xlsx',"Sheet1", False)
+input.readConnectionsFromExcel(mymodel,'ExpN_readconnectionsstruct_Excel.xlsx',"Sheet1", True)
 
 mymodel.printStruct()
--- Examples/Dictionary/Input/dictExpN_readStructFromExcel.py
+++ Examples/Dictionary/Input/dictExpN_readStructFromExcel.py
@@ -6,6 +6,6 @@ from AhpAnpLib import calcs_AHPLib as calc
 #create model
 mymodel=str.Model("My Model")
 
-input.readStructFromExcel(mymodel,'ExpN_readStruct_Excel.xlsx',"Sheet1", False)
+input.readStructFromExcel(mymodel,'ExpN_readStruct_Excel.xlsx',"Sheet1", True)
 
 mymodel.printStruct()
