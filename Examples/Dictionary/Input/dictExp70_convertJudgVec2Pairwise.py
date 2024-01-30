8e0b72de545357bbac95586166bee460efbe1643
Lirong <34753141+weileerong@users.noreply.github.com>
2024-01-30 19:40:51 +0800

    update example convertjudgvec2pairwise

--- Examples/Dictionary/Input/dictExp70_convertJudgVec2Pairwise.py
+++ Examples/Dictionary/Input/dictExp70_convertJudgVec2Pairwise.py
@@ -1,4 +1,3 @@
 from AhpAnpLib import inputs_AHPLib as input
 
-pw1=input.convertJudgVec2Pairwise([1,2,3,4,0.5,0.6],[0,1,2,3,4,5],4)
-print(pw1)
+pw1=input.convertJudgVec2Pairwise([0.2,0.3,0.4,0.5,0.6,0.7],[2,0,1,1,0,2,3,3],4)
