2dd6532b4bb4803e76cf7b6f5ade447715ede162
Lirong <34753141+weileerong@users.noreply.github.com>
2024-02-02 19:43:30 +0800

    update dict example getRealPosofNode

--- Examples/Dictionary/Input/dictExp_getRealPosOfNode.py
+++ Examples/Dictionary/Input/dictExp_getRealPosOfNode.py
@@ -2,5 +2,5 @@ from AhpAnpLib import inputs_AHPLib as input
 
 pos2=input.getRealPosOfNode([2,0,1,1,0,2,3,3],2)
 pos3=input.getRealPosOfNode([2,0,1,1,0,2,3,3],3)
-print("positiong of node 2:",pos2)
-print("positiong of node 3:",pos3)
+print("position of node 2:",pos2)
+print("position of node 3:",pos3)
